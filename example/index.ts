bcca1bc60666144564f5f42a8bcf085babe4c967
soulsam480 <soulsam480@hotmail.com>
2021-06-03 11:44:09 +0530

    chore: npm release v0.0.2

--- example/index.ts
+++ example/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
-import { createExpressServer } from '@dango/core';
+import { createExpressServer } from 'dango-core';
 const app = express();
 
 createExpressServer(app, {
   controllers: ['controllers/**.ts'],
-}).listen(3000);
+}).listen(3000, () => console.log('http://localhost:3000'));
